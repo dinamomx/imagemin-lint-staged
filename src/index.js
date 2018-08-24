b13b0d1618685e9cd3324050db406a84f6d5c366
ceasol <cesasol@hotmail.com>
2018-08-23 21:07:42 -0500

    chore(lib): compile from src with `babel-multi-env`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 "use strict";
 const fs = require("fs");
-const promisify = require("util.promisify");
+const {
+  promisify
+} = require("util");
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
@@ -27,13 +29,14 @@ const plugins = [
   [
     "imagemin-svgo",
     {
-      plugins: [{ removeViewBox: false }]
+      plugins: [{
+        removeViewBox: false
+      }]
     }
   ]
 ].map(([name, opts]) => require(name)(opts));
 
-export const minifyFile = filename =>
-  [...plugins, it => writeFile(filename, it)].reduce(
-    (acc, it) => acc.then(it),
-    readFile(filename)
-  );
+export const minifyFile = filename => [...plugins, it => writeFile(filename, it)].reduce(
+  (acc, it) => acc.then(it),
+  readFile(filename)
+);
